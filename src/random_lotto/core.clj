1680a2866619f4c5cbb2f3c15bdd461a14d7b29c
Lewis Yearsley <lewis.yearsley@arcadiagroup.co.uk>
2019-03-29 14:28:01 +0000

    Fix duplicate issue

--- src/random_lotto/core.clj
+++ src/random_lotto/core.clj
@@ -1,15 +1,21 @@
 (ns random-lotto.core
   (:gen-class))
 
+(defn in 
+  "Checks if a value is in a collection"
+  [collection value]
+    (some #(= value %) collection))
+
 (defn generate-numbers
   "Generates the random collection of numbers"
   [generated]    
   (let [max 59]
     (if (= (count generated) 6)
       (sort generated)
+      ;given that this is tail recursion it shouldn't overflow the stack      
       (generate-numbers 
-        (let [num (+ 1 (rand-int max))]
-          (if (contains? generated num)
+        (let [num (+ 1 (rand-int max))]          
+          (if (in generated num)
             generated
             (conj generated num)))))))
 
